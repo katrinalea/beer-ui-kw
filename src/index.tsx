c8623e52eb943fea292df07fb587fc5e65bd9977
Katrina <katrinalea.woolley@yahoo.co.uk>
2023-01-23 14:13:56 +0000

    responsive design

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,7 @@ import "./style.css";
 
 ReactDOM.render(
   <React.StrictMode>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
     <App />
   </React.StrictMode>,
   document.getElementById("root")
